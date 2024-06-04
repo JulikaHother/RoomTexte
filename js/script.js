9cf53a8d958b513ebc76762f6791eca6fe25ab2a
JulikaHother <julika.hother@gmx.de>
2024-06-04 16:28:18 +0200

    skalierung überarbeitet, farben

--- js/script.js
+++ js/script.js
@@ -1,90 +1,154 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const text = `sind Ruth-Maria Thomas und Cynthia Cornelius.
-
-// Sie sind am 15.12.1993 geboren und in Cottbus aufgewachsen. Sie schreiben allein und seit 2021 gemeinsam für Kunstausstellungen und Magazine.
-
-// Ruth-Maria Thomas ist Sozialarbeiterin und hat am Literaturinstitut in Leipzig studiert. Zuletzt erschienen ihre Essays „wie ich frau bin“ bei SUKULTUR und „toxic“ bei SWR2. Ihr Debütroman erscheint 2024 bei Rowohlt.
-
-// Cynthia Cornelius hat Literaturwissenschaften und Französisch in Marseille und Halle (Saale) studiert. Für das ZEITmagazin veröffentlichte sie diverse Kolumnen, Interviews und eine Fotostrecke. Außerdem ist sie Mitherausgeberin des literarischen Groschenromans Hot Topic!.`;
-//   let index = 0;
-//   const typingSpeed = 100; // Millisekunden zwischen jedem Buchstaben
-
-//   function typeText() {
-//     if (index < text.length) {
-//       document.getElementById('typed-text').textContent += text.charAt(index);
-//       index++;
-//       setTimeout(typeText, typingSpeed);
-//     }
-//   }
-
-// const textPubli = `erschienen in ...
-// processin_magazine
-// HONICH.VERSION
-// Heide Nord
-// Ausstellung Johannes XY
-// Ausstellung Selma XY
-// Magazin XYZ
-// Magazin XYZ
-// Magazin XYZ`;
-
-// let indexPubli = 0;
-// const typingSpeedPubli = 100; // Millisekunden zwischen jedem Buchstaben, anpassbar
-
-// function typeTextPubli() {
-//   if (indexPubli < textPubli.length) {
-//     document.getElementById('typed-text-publi').textContent += textPubli.charAt(indexPubli);
-//     indexPubli++;
-//     setTimeout(typeTextPubli, typingSpeedPubli);
-//   }
-// }
-
-
-
-//   function updateViewportSize() {
-//     var width = window.innerWidth || document.documentElement.clientWidth;
-//     var height = window.innerHeight || document.documentElement.clientHeight;
-
-//     document.getElementById('viewport-size').innerHTML = `
-//       Room ${width} x ${height}<br><br>
-//       Room 1 <span class="wer">wer</span><br>
-//       Room 2 <span class="wo">wo</span><br>
-//       Room 3 <span class="wie">wie</span><br>
-//       Room 4 <span class="was">was</span><br>
-//     `;
-//   }
-
-//   // Starten der Textanimation
-//   typeText();
-
-//   // Aktualisieren beim Laden der Seite
-//   updateViewportSize();
-
-//   // Aktualisieren, wenn die Größe des Fensters geändert wird
-//   window.onresize = updateViewportSize;
-// });
-
-function applyTypewriterEffect(elementId, loop = true, pauseDuration = 2500, deleteDuration = 10000) {
+// Typewriter effect initialization
+function applyTypewriterEffect(elementId, pauseDuration = 10) {
   var element = document.getElementById(elementId);
   var html = element.innerHTML;
 
-  // Ersetze mehrere aufeinanderfolgende Leerzeichen und Zeilenumbrüche durch ein einziges Leerzeichen
+  // Clean up the HTML content by replacing multiple spaces and line breaks with a single space
   var cleanedHtml = html.replace(/\s*(<br\s*\/?>)\s*/gi, '$1').replace(/\s\s+/g, ' ').trim();
 
-  var typewriter = new Typewriter(element, {
-    loop: loop
-  });
+  // Initialize the Typewriter effect
+  var typewriter = new Typewriter(element, { loop: false });
 
   typewriter.pauseFor(pauseDuration)
-    .typeString(cleanedHtml)
-    .pauseFor(pauseDuration)
-    .deleteAll()
-    .pauseFor(deleteDuration)
     .typeString(cleanedHtml)
     .start();
 }
 
+// Apply the Typewriter effect to specified elements
 applyTypewriterEffect('about-text');
 applyTypewriterEffect('typed-text-publi');
 applyTypewriterEffect('typed-text');
+applyTypewriterEffect('kontakt');
 
 
+
+
+
+$(document).ready(function () {
+  const documentWidth = $(document).width();
+  console.log('Document width (initial):', documentWidth);
+
+  function checkWidth() {
+    const windowWidth = $(window).width();
+    console.log('Window width:', windowWidth);
+
+    if (windowWidth <= documentWidth) {
+      $('body').addClass('halb-0');
+      $('body').removeClass('halb-3 halb-2 halb');
+      $('.halb-0').show();
+      $('.halb-3, .halb-2, .halb').hide();
+      $('.raumblau').css('background-color', '');
+      $('.raumbraun').css('background-color', '');
+      $('.raumorange').css('background-color', '');
+    } else {
+      $('body').removeClass('halb-0');
+      $('.halb-0').hide();
+    }
+
+    if (windowWidth <= documentWidth / 1.1) {
+      $('body').addClass('halb-3');
+      $('body').removeClass('halb-2 halb halb-0');
+      $('.halb-3').show();
+      $('.halb-2, .halb, .halb-0').hide();
+      $('.raumblau').css('background-color', 'rgba(0, 140, 255, 0.41)');
+      $('.raumbraun').css('background-color', '');
+      $('.raumorange').css('background-color', '');
+    } else {
+      $('body').removeClass('halb-3');
+      $('.halb-3').hide();
+      $('.raumblau').css('background-color', '');
+    }
+
+    if (windowWidth <= documentWidth / 1.5) {
+      $('body').addClass('halb-2');
+      $('body').removeClass('halb-3 halb halb-0');
+      $('.halb-2').show();
+      $('.halb-3, .halb, .halb-0').hide();
+      $('.raumblau').css('background-color', '');
+      $('.raumbraun').css('background-color', 'rgb(179, 134, 105, 0.41)');
+      $('.raumorange').css('background-color', '');
+    } else {
+      $('body').removeClass('halb-2');
+      $('.halb-2').hide();
+      $('.raumbraun').css('background-color', '');
+    }
+
+    if (windowWidth <= documentWidth / 2) {
+      $('body').addClass('halb');
+      $('body').removeClass('halb-3 halb-2 halb-0');
+      $('.halb').show();
+      $('.halb-2, .halb-3, .halb-0').hide();
+      $('.raumblau').css('background-color', '');
+      $('.raumbraun').css('background-color', '');
+      $('.raumorange').css('background-color', 'rgb(255, 110, 77, 0.41)');
+    } else {
+      $('body').removeClass('halb');
+      $('.halb').hide();
+      $('.raumorange').css('background-color', '');
+    }
+  }
+
+  // Initial check
+  checkWidth();
+
+  // Check width on window resize
+  $(window).resize(function () {
+    checkWidth();
+  });
+
+  const documentHeight = $(document).height();
+  console.log('Document height (initial):', documentHeight);
+
+  function checkHeight() {
+    const windowHeight = $(window).height();
+    console.log('Window height:', windowHeight);
+
+    if (windowHeight <= documentHeight) {
+      $('body').addClass('hoehe1');
+      $('body').removeClass('hoehe2 hoehe3 hoehe4');
+      $('.hoehe1').show();
+      $('.hoehe2, .hoehe3, .hoehe4').hide();
+    } else {
+      $('body').removeClass('hoehe1');
+      $('.hoehe1').hide();
+    }
+
+    if (windowHeight <= documentHeight / 1.2) {
+      $('body').addClass('hoehe2');
+      $('body').removeClass('hoehe1 hoehe3 hoehe4');
+      $('.hoehe2').show();
+      $('.hoehe1, .hoehe3, .hoehe4').hide();
+    } else {
+      $('body').removeClass('hoehe2');
+      $('.hoehe2').hide();
+    }
+
+    if (windowHeight <= documentHeight / 1.5) {
+      $('body').addClass('hoehe3');
+      $('body').removeClass('hoehe1 hoehe2 hoehe4');
+      $('.hoehe3').show();
+      $('.hoehe1, .hoehe2, .hoehe4').hide();
+    } else {
+      $('body').removeClass('hoehe3');
+      $('.hoehe3').hide();
+    }
+
+    if (windowHeight <= documentHeight / 1.8) {
+      $('body').addClass('hoehe4');
+      $('body').removeClass('hoehe1 hoehe2 hoehe3');
+      $('.hoehe4').show();
+      $('.hoehe1, .hoehe2, .hoehe3').hide();
+    } else {
+      $('body').removeClass('hoehe4');
+      $('.hoehe4').hide();
+    }
+  }
+
+  // Initial check
+  checkHeight();
+
+  // Check height on window resize
+  $(window).resize(function () {
+    checkHeight();
+  });
+});
